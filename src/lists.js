c856c0bb5a0b9ceaa00d0a6294d1ae134303f4e4
aungkomin7 <winfloweraungkomin1997@gmail.com>
2025-05-23 19:57:48 +0700

    set main sm padding

--- src/lists.js
+++ src/lists.js
@@ -55,9 +55,7 @@ export const editList = (listId) => {
     "border-b",
     "px-1",
     "focus-visible:outline-none",
-    "max-sm:focus-visible:outline-none",
     "max-w-[230px]",
-    "max-sm:w-[180px]",
     "font-mono"
   );
   taskInfo.after(newTaskInput);
